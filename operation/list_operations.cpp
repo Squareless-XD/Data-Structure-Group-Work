70436ba0f8f7ae5c23fa26ab0d3c39ce5e55806e
Squareless-XD <1571676085@qq.com>
2023-06-02 19:44:31 +0800

    add new content to README

--- operation/list_operations.cpp
+++ operation/list_operations.cpp
@@ -1,8 +1,5 @@
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <time.h>
+#include <bits/stdc++.h>
+
 #define ElemType int
 
 // claim a variable called "struct LNode"
