d7f9db0945db927a045995118a0a1deafaa0f352
孔德衍 <134758468+ForwarDerCN@users.noreply.github.com>
2023-06-03 23:33:12 +0800

    Basic Trigger with path drawing
    It seems hard to connect with the algorithm

---
+++ knight's-tour-problem/knight_run.cpp
@@ -0,0 +1,15 @@
+#include "Knight_draw.h"
+//#include "Knight_algorithm.h"
+
+//extern int tour[BOARD_SIZE_SQUARE][2];
+//extern int SetX, SetY;
+
+int main()
+{
+	PrintFirst();
+	Onclick();
+	//Algorithm_op();
+	EndPrint();
+	_getch();
+	return 0;
+}
